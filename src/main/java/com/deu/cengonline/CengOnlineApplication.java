e81f26a0e1a8799d2b453801f08320ef190dd2d7
serhatyuna <serhat.yuna@ceng.deu.edu.tr>
2020-04-25 14:19:37 +0300

    initial commit

---
+++ src/main/java/com/deu/cengonline/CengOnlineApplication.java
@@ -0,0 +1,11 @@
+package com.deu.cengonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CengOnlineApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CengOnlineApplication.class, args);
+	}
+}
---
+++ src/test/java/com/deu/cengonline/CengOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deu.cengonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CengOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
